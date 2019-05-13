c847abdd7b664e1b7e800ff0e64d1c35f779b0ab
Ronildo Braga Junior <ronildo.braga.junior@gmail.com>
2019-05-13 15:53:59 +1000

    fix

--- smsf/mailgun.py
+++ smsf/mailgun.py
@@ -1,15 +1,18 @@
+import requests
+
 MAILGUN_DOMAIN_NAME = 'squirrelsuper.com.au'
 MAILGUN_API_KEY = 'd4a62ee74f4d93e4e6bb92e27e1a4b77-6b60e603-dec213b5'
 
 def send_account_activation(to):
     url = 'https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN_NAME)
-    auth = ('api', MAILGUN_API_KEY)
+
+    f = open('resources/templates/account_activation/content.txt','r')
+
     data = {
         'from': 'Mailgun User <save@{}>'.format(MAILGUN_DOMAIN_NAME),
         'to': to,
-        'subject': 'Simple Mailgun Example',
-        'text': 'Plaintext content',
+        'subject': 'Squirrel account activation',
+        'text': f.read(),
     }
 
-    # response = requests.post(url, auth=auth, data=data)
-    # response.raise_for_status()
+    requests.post(url, auth=('api', MAILGUN_API_KEY), data=data)
